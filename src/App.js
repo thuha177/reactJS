5fe12544e1b6e57e6c31f21f3c0aa6f9f9ebc58e
Ha Nguyen <nguyenthithuha177@gmail.com>
2021-08-29 20:44:10 +0700

    render elements in reactjs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const name = 'Ha';
+  const age = 18;
+  const isFemale = true;
+  const student = {
+    name: 'Easy Frontend'
+  };
+  const colourList = ['red', 'green', 'blue']
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ha Nguyen
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Xin chao {name} - {age} - {isFemale ? 'Female' : 'Male'} </p>
+        {isFemale ? <p>Female</p> : <p>Male</p>}
+
+        {isFemale && <p>Female</p>}
+        {!isFemale && <p>Male</p>}
+
+        {isFemale && (
+          <div>
+            <p>Female 1</p>
+            <p>Female 2</p>
+            <p>Female 3</p>
+          </div>
+
+        )}
+        {isFemale && (
+          <React.Fragment>
+            <p>Female 1</p>
+            <p>Female 2</p>
+            <p>Female 3</p>
+          </React.Fragment>
+
+        )}
+
+        {isFemale && (
+          <>
+            <p>Female 1</p>
+            <p>Female 2</p>
+            <p>Female 3</p>
+          </>
+
+        )}
+
+        <p>{student.name}</p>
+
+        <ul>
+          {colourList.map(color => (
+            <li style={{ color }}>{color}</li>
+          ))}
+        </ul>
+
       </header>
     </div>
   );
