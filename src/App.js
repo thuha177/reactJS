43ba9479a7a5a45eba33f642392cb9c610de18b6
Ha Nguyen <nguyenthithuha177@gmail.com>
2021-09-02 23:47:28 +0700

    LabAlbumList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-// import './App.css';
 import React from 'react';
-import TodoFeature from './features/Todo';
+import AlbumFeature from './features/Album';
 
 function App() {
   return (
     <div className="App">
-      <TodoFeature />
+      <AlbumFeature />
     </div>
   );
 }
