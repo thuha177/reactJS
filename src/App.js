e07eada73cfd49e4afe938cb3a087620ef631faf
Ha Nguyen <nguyenthithuha177@gmail.com>
2021-09-02 22:15:47 +0700

    Todolist

--- src/App.js
+++ src/App.js
@@ -1,64 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import React from 'react';
+import TodoFeature from './features/Todo';
 
 function App() {
-  const name = 'Ha';
-  const age = 18;
-  const isFemale = true;
-  const student = {
-    name: 'Easy Frontend'
-  };
-  const colourList = ['red', 'green', 'blue']
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ha Nguyen
-        </p>
-        <p>Xin chao {name} - {age} - {isFemale ? 'Female' : 'Male'} </p>
-        {isFemale ? <p>Female</p> : <p>Male</p>}
-
-        {isFemale && <p>Female</p>}
-        {!isFemale && <p>Male</p>}
-
-        {isFemale && (
-          <div>
-            <p>Female 1</p>
-            <p>Female 2</p>
-            <p>Female 3</p>
-          </div>
-
-        )}
-        {isFemale && (
-          <React.Fragment>
-            <p>Female 1</p>
-            <p>Female 2</p>
-            <p>Female 3</p>
-          </React.Fragment>
-
-        )}
-
-        {isFemale && (
-          <>
-            <p>Female 1</p>
-            <p>Female 2</p>
-            <p>Female 3</p>
-          </>
-
-        )}
-
-        <p>{student.name}</p>
-
-        <ul>
-          {colourList.map(color => (
-            <li style={{ color }}>{color}</li>
-          ))}
-        </ul>
-
-      </header>
+      <TodoFeature />
     </div>
   );
 }
